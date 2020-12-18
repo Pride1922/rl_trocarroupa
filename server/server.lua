9daf5c6d182af9b6507f8e3db97cc443d0043319
Pride1922 <48358139+Pride1922@users.noreply.github.com>
2020-12-18 22:54:16 +0100

    Create server.lua

---
+++ server/server.lua
@@ -0,0 +1,41 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+ESX.RegisterServerCallback('rl_trocarroupa:getPlayerDressing', function(source, cb)
+	local xPlayer  = ESX.GetPlayerFromId(source)
+
+	TriggerEvent('esx_datastore:getDataStore', 'property', xPlayer.identifier, function(store)
+		local count  = store.count('dressing')
+		local labels = {}
+
+		for i=1, count, 1 do
+			local entry = store.get('dressing', i)
+			table.insert(labels, entry.label)
+		end
+
+		cb(labels)
+	end)
+end)
+
+ESX.RegisterServerCallback('rl_trocarroupa:getPlayerOutfit', function(source, cb, num)
+	local xPlayer  = ESX.GetPlayerFromId(source)
+
+	TriggerEvent('esx_datastore:getDataStore', 'property', xPlayer.identifier, function(store)
+		local outfit = store.get('dressing', num)
+		cb(outfit.skin)
+	end)
+end)
+
+
+RegisterServerEvent('rl_trocarroupa:removeOutfit')
+AddEventHandler('rl_trocarroupa:removeOutfit', function(label)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	TriggerEvent('esx_datastore:getDataStore', 'property', xPlayer.identifier, function(store)
+		local dressing = store.get('dressing') or {}
+
+		table.remove(dressing, label)
+		store.set('dressing', dressing)
+	end)
+end)
