501b53fd19529f1dfe46a348d099d55d9f628df7
Pride1922 <48358139+Pride1922@users.noreply.github.com>
2020-12-18 22:53:29 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,35 @@
+Config = {}
+
+Config.DrawDistance = 100.0
+Config.MarkerSize   = {x = 0.3, y = 0.3, z = 0.3}
+Config.MarkerColor  = {r = 0, g = 0, b = 204}
+Config.MarkerType   = 2
+
+Config.Markers = {
+	Policia = {
+		trabalho = 'police',
+		offtrabalho = 'offpolice',
+		coords = vector3(457.134, -988.3648, 30.67834),
+	},
+	Mecanico = {
+		trabalho = 'mechanic',
+		offtrabalho = 'offmechanic',
+		coords = vector3(-231.956, -1331.314, 31.167),
+	},
+	Vinha = {
+		trabalho = 'vigne',
+		offtrabalho = 'offvigne',
+		coords = vector3(-1879.556, 2066.506, 145.5604),
+	},
+	Inem = {
+		trabalho = 'ambulance',
+		offtrabalho = 'offambulance',
+		coords = vector3(375.6528, -1434.21, 32.49816),
+	},
+	Yakuza = {
+		trabalho = 'yakuza',
+		offtrabalho = 'offyakuza',
+		coords = vector3(1501.754, 111.3494, 32.4981),
+	},
+}
+
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'RedLine Trocar de Roupa nos trabalhos'
+author 'Pride1922'
+version '1.0.0'
+
+server_scripts {
+    'config.lua',
+    'server/server.lua'
+}
+
+client_scripts {
+    'config.lua',
+    'client/client.lua'
+}
+
+dependencies {
+    'es_extended'
+}
