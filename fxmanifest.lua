a345713e56fe0cbaff1a26fecd203f15d3d4939d
Pride1922 <48358139+Pride1922@users.noreply.github.com>
2020-12-22 13:05:20 +0100

    Update fxmanifest.lua
    Added locale pt

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,20 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'RedLine Trocar de Roupa nos trabalhos'
+description 'Change clothing at workplace'
 author 'Pride1922'
 version '1.0.0'
 
 server_scripts {
     'config.lua',
-    'server/server.lua'
+    'server/server.lua',
+	'locales/pt.lua'
 }
 
 client_scripts {
     'config.lua',
-    'client/client.lua'
+    'client/client.lua',
+	'locales/pt.lua'
 }
 
 dependencies {
