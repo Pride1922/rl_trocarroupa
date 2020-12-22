e659becafea1f3220030271212db012a00455430
Pride1922 <48358139+Pride1922@users.noreply.github.com>
2020-12-22 13:04:33 +0100

    Create pt.lua

---
+++ locales/pt.lua
@@ -0,0 +1,8 @@
+Locales['pt'] = {
+  ['Marker_text'] = '~g~E~w~ - Trocar de roupa', -- Change clothes
+  ['Menu_Title1'] = 'Roupeiro', -- Closet
+  ['Menu_Title2'] = 'Roupas', -- Clothes
+  ['Menu_Title3'] = 'Remover Roupas', -- Remove Clothes 
+  ['Change_Clothes'] = 'Trocar Roupa', -- Change Clothes
+  ['Remove_Clothes'] = 'Remover Roupa', -- Remove Clothes
+}
